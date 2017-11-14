039d08314ca54d2d5f16696202c2956f2ea9ee26
kegplant <ace1905@gmail.com>
2017-11-14 11:15:03 -0800

    initial commit

---
+++ apps/survey/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from . import views #this line is new! #imports views.py from current folder
+urlpatterns=[
+    url(r'^$', views.index),#this line has changed!,
+    url(r'^process$',views.process),
+    url(r'^result$',views.result)
+]
